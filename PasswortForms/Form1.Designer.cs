f461286f3ec15bd77df78d09b2238aba6aeb9041
sha <66492970+shakeeu@users.noreply.github.com>
2020-12-06 20:31:54 +0100

    Projektdateien hinzufügen.

---
+++ PasswortForms/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PasswortForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelNummer = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxNeuPass = new System.Windows.Forms.TextBox();
+            this.textBoxBestPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SandyBrown;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passwortverwaltung";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(125, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 30);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(124, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Programmende";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelNummer
+            // 
+            this.labelNummer.AutoSize = true;
+            this.labelNummer.BackColor = System.Drawing.Color.Yellow;
+            this.labelNummer.Location = new System.Drawing.Point(468, 111);
+            this.labelNummer.Name = "labelNummer";
+            this.labelNummer.Padding = new System.Windows.Forms.Padding(12, 5, 12, 5);
+            this.labelNummer.Size = new System.Drawing.Size(65, 23);
+            this.labelNummer.TabIndex = 3;
+            this.labelNummer.Text = "label2";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.BackColor = System.Drawing.Color.Yellow;
+            this.labelName.Location = new System.Drawing.Point(468, 148);
+            this.labelName.Name = "labelName";
+            this.labelName.Padding = new System.Windows.Forms.Padding(12, 5, 12, 5);
+            this.labelName.Size = new System.Drawing.Size(65, 23);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "label3";
+            // 
+            // textBoxNeuPass
+            // 
+            this.textBoxNeuPass.Location = new System.Drawing.Point(415, 216);
+            this.textBoxNeuPass.Name = "textBoxNeuPass";
+            this.textBoxNeuPass.Size = new System.Drawing.Size(116, 21);
+            this.textBoxNeuPass.TabIndex = 5;
+            // 
+            // textBoxBestPass
+            // 
+            this.textBoxBestPass.Location = new System.Drawing.Point(415, 299);
+            this.textBoxBestPass.Name = "textBoxBestPass";
+            this.textBoxBestPass.Size = new System.Drawing.Size(116, 21);
+            this.textBoxBestPass.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(429, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Passwort Neu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Passwort bestätigen";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(415, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Passwort übernehmen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxBestPass);
+            this.Controls.Add(this.textBoxNeuPass);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelNummer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelNummer;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxNeuPass;
+        private System.Windows.Forms.TextBox textBoxBestPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PasswortForms/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using PasswortBibliothek;
+
+namespace PasswortForms
+{
+    public partial class Form1 : Form
+    {
+
+        PasswortVerwaltung mitarbeiter = new PasswortVerwaltung();
+        OleDbDataReader dataReader = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataReader = mitarbeiter.laden();
+            while (dataReader.Read())
+            {
+                listBox1.Items.Add(dataReader.GetInt32(0).ToString());
+            }
+            mitarbeiter.close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataReader = mitarbeiter.select(Convert.ToInt32(listBox1.SelectedItem));
+            dataReader.Read();
+
+            labelNummer.Text = dataReader.GetInt32(0).ToString();
+            labelName.Text = dataReader.GetString(1);
+            mitarbeiter.close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBoxNeuPass.Text == textBoxBestPass.Text)
+            {
+                mitarbeiter.update(textBoxNeuPass.Text, Convert.ToInt32(listBox1.SelectedItem));
+                MessageBox.Show("Passwort erfolgreich geändert.");
+            }
+            else
+               MessageBox.Show("Passwörter stimmen nicht überein!", "Fehlermeldung", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+  
+            mitarbeiter.close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
